c61714a32d11f6408fb3b9943e3bfc0120a86737
OmarShaher <omarshaher2011@gmail.com>
2023-12-02 18:07:29 +0200

    added stores location

---
+++ index.js
@@ -0,0 +1,7 @@
+function goToStores() {
+    // Replace 'YOUR_LINK_HERE' with the link you want to redirect to
+    var redirectTo = 'https://pan-marine.net/images/2020/06/18/pm-map.jpg'; // Example link
+    
+    // Open the new link in a new tab
+    window.open(redirectTo, '_blank');
+}
