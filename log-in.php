5b4666fbb3227db469ef7e49bc734571c282acb6
OmarShaher <omarshaher2011@gmail.com>
2023-12-14 11:35:21 +0200

    added sign up

--- log-in.php
+++ log-in.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Admin Login</title>
+    <title> Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="Texx.css">
@@ -16,7 +16,7 @@
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <form class="login-form" method="post">
-                    <h2 class="text-center mb-4">Admin Login</h2>
+                    <h2 class="text-center mb-4"> Login</h2>
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" class="form-control" name="user" id="username" required>
@@ -26,6 +26,8 @@
                         <input type="password" class="form-control" name="pwd" id="password" required>
                     </div>
                     <button type="submit" name="btn" class="btn btn-primary btn-block">Login</button>
+                    <!-- Add Signup Button -->
+                <button type="button" class="btn btn-secondary btn-block mt-3" onclick="location.href='signin.php';">Sign Up</button>
                 </form>
             </div>
         </div>
--- signin.php
+++ signin.php
@@ -1,58 +1,93 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Sign In</title>
-</head>
-<body>
-    <h2>Sign In</h2>
-    <form action="" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Sign In">
-    </form>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <title>User Sign Up </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="signin.css">
+</head>
+
+<body>
+    
+    <div class="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <form class="signup-form" method="post">
+                    <h2 class="text-center mb-4">Sign Up</h2>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Enter your name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" name="user" id="username" placeholder="Enter your Username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="pwd1" placeholder="Enter your password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirmPassword" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="confirmPassword" name="pwd2" placeholder="Confirm Password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your Email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="gender" class="form-label">Gender</label>
+                        <select class="form-select" name="G" id="gender">
+                            <option value="" disabled selected>Select Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block" name="btn">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Database configuration
-        $host = 'localhost'; // Assuming XAMPP is running on your local machine
-        $dbname = 'businessdb'; // Database name
-        $username_db = 'root'; // Database username
-        $password_db = ''; // Database password is empty (null) by default in XAMPP
-        
-        try {
-            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username_db, $password_db);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            // Retrieve username and password from the form
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            
-            // Prepare a query to fetch the user's credentials from the database
-            $query = "SELECT * FROM user WHERE Username = :username AND Password = :password";
-            $stmt = $conn->prepare($query);
-            $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':password', $password); // In practice, passwords should be hashed and compared securely
-            
-            // Execute the query
-            $stmt->execute();
-            
-            // Fetch the user data
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            
-            if ($user) {
-                // User found, show a confirmation
-                echo "<p>Login successful for user: " . $user['Username'] . "</p>";
-                // Additional logic or display here
-            } else {
-                // User not found, show error message
-                echo "<p>Invalid username or password.</p>";
-            }
-        } catch(PDOException $e) {
-            echo "<p>Connection failed: " . $e->getMessage() . "</p>";
+     if(isset($_POST['btn'])){
+        $name=$_POST["name"];
+        $usr=$_POST["user"];
+        $pwd1=$_POST["pwd1"];
+        $pwd2=$_POST["pwd2"];
+        $email=$_POST["email"];
+        $gender=$_POST["G"];
+
+        if($name==null || $usr==null || $pwd1==null || $pwd2==null || $email==null || $gender==null){
+            echo"<h1>Fill all required fields</h1><br>";
+            echo"<h1>Sign up Again</h1>";
+        }elseif($pwd1 != $pwd2){
+        echo"<h1>Passwords are Mismatched</h1><br>";
+        echo"<h1>Sign up Again</h1>";
+        }else{
+            $conn=mysqli_connect("localhost","root","","businessdb");
+            $stmt="INSERT INTO `user` (`Name`,`Username`,`Password`,`Email`,`Gender`)
+            VALUES('$name','$usr','$pwd1','$email','$gender')";
+            $result=mysqli_query($conn, $stmt);
+        if($result==FALSE){
+            echo"<h1>Error. $name was not added</h1>";
         }
-    }
+        else{
+            echo"<h1>$name was added Successfully</h1>";
+        }
+        }}
     ?>
+    <footer class="bg-dark text-light text-center py-3">
+   
+</footer>
 </body>
+
 </html>
