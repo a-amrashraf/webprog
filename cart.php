b68eed65f6de3a46c0089147b83538e962dce7bf
OmarShaher <omarshaher2011@gmail.com>
2023-12-15 18:00:41 +0200

    added welcome after login

--- cart.php
+++ cart.php
@@ -2,9 +2,27 @@
 <html>
 <head>
     <title>Shopping Cart</title>
-</head>
+    <link rel="stylesheet" href="Test.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
 <body>
-
+<div class="info">
+            <div class="blackRectangle">
+                <span class="icon"><i class="fa fa-phone" aria-hidden="true"></i></span>
+                <span class="text">19255</span>
+                <span class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                    <a href="https://pan-marine.net/images/2020/06/18/pm-map.jpg" class="text">Our Stores </a>
+                </span>               
+                 <span class="icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+               <a href="cart.php" class="text">My Cart</a>
+                <a href="log-in.php">
+                 <span class="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                </a>
+            </div>
+        </div>
+         
+        <h1 class="name">MY CART</h2><br>
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['fetch_data']) && $_GET['fetch_data'] === 'true') {
     echo '<div id="cartInfo">';
@@ -34,6 +52,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['fetch_data']) && $_GET[
     echo '</div>';
 } 
 ?>
-
+<button onclick="goToIndex()">Return to Index</button>
+<script>
+    function goToIndex() {
+        window.location.href = 'index.html';
+    }
+</script>
 </body>
 </html>
--- log-in.php
+++ log-in.php
@@ -3,11 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title> Login</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="Texx.css">
-
 </head>
 
 <body>
@@ -16,7 +15,7 @@
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <form class="login-form" method="post">
-                    <h2 class="text-center mb-4"> Login</h2>
+                    <h2 class="text-center mb-4">Login</h2>
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" class="form-control" name="user" id="username" required>
@@ -26,9 +25,29 @@
                         <input type="password" class="form-control" name="pwd" id="password" required>
                     </div>
                     <button type="submit" name="btn" class="btn btn-primary btn-block">Login</button>
-                    <!-- Add Signup Button -->
-                <button type="button" class="btn btn-secondary btn-block mt-3" onclick="location.href='signin.php';">Sign Up</button>
+                    <button type="button" class="btn btn-secondary btn-block mt-3"
+                        onclick="location.href='signin.php';">Sign Up</button>
                 </form>
+                <!-- Display the retrieved username -->
+                <?php
+                if (isset($_POST['btn'])) {
+                    $user = $_POST["user"]; // Update variable name
+                    $password = $_POST["pwd"]; // Update variable name
+                    
+                    $conn = mysqli_connect("localhost", "root", "", "businessdb");
+                    $stmt = "SELECT * from user where Username='$user' AND Password='$password'";
+                    $result = mysqli_query($conn, $stmt);
+                    if ($row = mysqli_fetch_array($result)) {
+                        // Store the username in a variable
+                        $retrievedUsername = $user;
+                        // Display a welcome message with the retrieved username
+                        echo "<h3>Welcome, $retrievedUsername!</h3>";
+                    } else {
+                        echo "<h4>Login failed. Username or password are incorrect</h4>";
+                    }
+                    mysqli_close($conn);
+                }
+                ?>
             </div>
         </div>
     </div>
@@ -37,26 +56,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-  <?php
-    if(isset($_POST['btn'])){
-        $user = $_POST["user"]; // Update variable name
-        $password = $_POST["pwd"]; // Update variable name
-        
-        $conn = mysqli_connect("localhost", "root", "", "businessdb");
-        $stmt = "SELECT * from user where Username='$user' AND Password='$password'";
-        $result = mysqli_query($conn, $stmt);
-        if($row = mysqli_fetch_array($result)){
-            // Redirect to index.html with username as a parameter
-            header("Location: index.html?username=" . urlencode($user));
-            exit(); // Make sure to exit after redirection
-        } else {
-            echo "<h1>Login failed. Username or password are incorrect</h1>";
-        }
-    }
-?>
-
-
-
 </body>
-
 </html>
