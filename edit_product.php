7477d378216bc086f16020e7c38207a1a9053557
IbrahimAbouzeid <152697040+IbrahimAbouzeid@users.noreply.github.com>
2023-12-25 17:19:37 +0200

    edit product updated

--- edit_product.php
+++ edit_product.php
@@ -3,9 +3,18 @@
 <html>
 <head>
     <title>Edit Product</title>
+    <link rel="stylesheet" href="Test.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <style>
+        body{
+        font-family:sans-serif;
+        font-size: 17px;
+    }
+    </style>
 </head>
 <body>
-    <h1>Edit Product</h1>
+    <h1 style="text-align:center;">Edit Product</h1>
 
     <?php
     include_once('connection.php');
@@ -37,20 +46,29 @@
             }
     ?>
             <!-- Form to edit the product -->
+            <div class="container">
+        <div class="row">
+       <div class="col-md-6 offset-md-3">
             <form action="" method="POST">
-                <label for="description">Description:</label>
-                <input type="text" id="description" name="description" value="<?php echo $row['description']; ?>" required><br><br>
-
-                <label for="price">Price:</label>
-                <input type="text" id="price" name="price" value="<?php echo $row['price']; ?>" required><br><br>
-
-                <label for="image_url">Image URL:</label>
-                <input type="text" id="image_url" name="image_url" value="<?php echo $row['image_url']; ?>" required><br><br>
-
-                <input type="submit" value="Update">
+            <div class="user mb-3">
+                <label for="description"  class="form-label">Description:</label>
+                <input type="text" class="form-control" id="description" name="description" value="<?php echo $row['description']; ?>" required><br><br>
+            </div>
+            <div class="user mb-3">
+                <label for="price"  class="form-label">Price:</label>
+                <input type="text" class="form-control" id="price" name="price" value="<?php echo $row['price']; ?>" required><br><br>
+            </div>
+            <div class="user mb-3">
+                <label for="image_url"  class="form-label">Image URL:</label>
+                <input type="text" class="form-control" id="image_url" name="image_url" value="<?php echo $row['image_url']; ?>" required><br><br>
+            </div>
+                <input class="add-tab-button" type="submit" value="Update">
             </form>
             <br>
             <a href="admin_dashboard.php"><button>Return to Dashboard</button></a>
+            </div>
+    </div>
+    </div>
     <?php
         } else {
             echo "Product not found.";
