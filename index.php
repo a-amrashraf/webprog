e2b9cfcf2add5c7665b88875692197b82afcafcd
OmarShaher <omarshaher2011@gmail.com>
2024-01-01 13:19:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -145,11 +145,9 @@ if(isset($_SESSION['username'])) {
             
 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
   </a>
   <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
   </a>
 </div>
 
